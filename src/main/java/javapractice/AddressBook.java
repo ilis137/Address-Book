6c718e873a75ce9eb463ccb2b64b3ac1b20ca64c
ilis137 <iliyasshahapure1@gmail.com>
2022-09-17 08:00:40 +0530

    initial commit-welcome message

---
+++ src/main/java/javapractice/AddressBook.java
@@ -0,0 +1,14 @@
+package javapractice;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class AddressBook 
+{
+    private static final Logger log = LogManager.getLogger(AddressBook.class);
+    public static void main( String[] args )
+    {
+        log.info("Welcome to Address Book Program");
+    }
+}
